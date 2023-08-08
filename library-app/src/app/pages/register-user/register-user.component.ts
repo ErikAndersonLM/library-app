21796ba15d4a0e6c89b33d0dd2f7e71c429d3dbd
Erik Anderson <erikanderson.lm@gmail.com>
2023-08-08 15:23:04 -0300

    craeate register user form

--- library-app/src/app/pages/register-user/register-user.component.ts
+++ library-app/src/app/pages/register-user/register-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  formRegisterUser!: FormGroup;
 
   ngOnInit(): void {
+
+    this.formRegisterUser = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
+      password:['', [Validators.required,Validators.minLength(8)]],
+      confirmPassword:['', [Validators.required,Validators.minLength(8)]]
+    });
+
   }
 
 }
