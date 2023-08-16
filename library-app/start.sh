ff41a25a4b5e97f12c4c55c0f6eb8d373fff34a0
Erik Anderson <erikanderson.lm@gmail.com>
2023-08-15 21:29:31 -0300

    remove unnecessary comment

--- library-app/start.sh
+++ library-app/start.sh
@@ -1,4 +1,3 @@
 echo "Script initialized..."
-nvm use 16.16.0 # Selecionando a versão compatível do NodeJS
 npm install # Instalação dos pacotes de dependências
 ng serve # Inicializando a aplicação Angular
